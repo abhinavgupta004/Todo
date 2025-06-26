318f8b8e9d922ae012e6354adc3ca48f649a85ae
abhinavgupta004 <abhimanyurock.2004@gmail.com>
2025-06-26 22:03:59 +0530

    Initial commit

---
+++ app2.js
@@ -0,0 +1,31 @@
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let inp = document.querySelector("input");
+btn.addEventListener("keypress",function(){
+let item = document.createElement("li");
+item.innerText = inp.value;
+
+let dltbtn = document.createElement("button");
+dltbtn.innerText = "delete";
+dltbtn.classList.add("delete");
+ item.appendChild(dltbtn);
+
+
+ul.appendChild(item);
+inp.value = "";
+});
+
+// let dltbtns = document.querySelectorAll(".delete");
+ 
+// for( dltbtn of dltbtns) {
+//     dltbtn.addEventListener("click", function() {
+//         let par = this.parentElement;
+//         par.remove();
+//     })
+// }
+ul.addEventListener("click", function(event) {
+    if(event.target.nodeName == "BUTTON") {
+        let listitem = event.target.parentElement;
+        listitem.remove();
+    }
+})
