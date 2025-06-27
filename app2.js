6a4b6acf5fafcaa577c38155c55c87b3bf190e65
abhinavgupta004 <abhimanyurock.2004@gmail.com>
2025-06-27 11:54:36 +0530

    Initial commit

--- app2.js
+++ app2.js
@@ -1,7 +1,7 @@
 let btn = document.querySelector("button");
 let ul = document.querySelector("ul");
 let inp = document.querySelector("input");
-btn.addEventListener("keypress",function(){
+btn.addEventListener("click",function(){
 let item = document.createElement("li");
 item.innerText = inp.value;
 
